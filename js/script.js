2e6620fd5e41fa6bf6250a299e359cb2badd9fb9
amberwlsn <arw89@uclive.ac.nz>
2017-02-08 21:38:38 +0000

    improved-site

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-    $('.Link').click(function(event) {
-        console.log(this);
-        console.log($(this).attr('href'));
-        event.preventDefault();
-        $(window).scrollTo($(this).attr('href'), 1000, { easing: 'easeInCubic' });
-        //$(window).scrollTo('li:eq(15)', 2500, {easing:'elasout'});
-
-
-    });
-
 
     $(".cross").hide();
     $(".menu").hide();
