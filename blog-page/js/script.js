5e344486b3eeb0f687783eaacc8286a30a3b462c
Amber <arw89@uclive.ac.nz>
2017-03-07 16:51:57 +0000

    Site V3D

---
+++ blog-page/js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function() {
+        $(".menu").slideToggle("slow", function() {
+            $(".hamburger").hide();
+            $(".cross").show();
+        });
+    });
+
+
+
+
+    $(".cross").click(function() {
+        $(".menu").slideToggle("slow", function() {
+            $(".cross").hide();
+            $(".hamburger").show();
+        });
+    });
+});
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-93042910-1', 'auto');
+  ga('send', 'pageview');
+
+  $(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 400;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+
+
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
